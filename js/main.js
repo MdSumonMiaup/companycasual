ab62c4a9d1a1b1b52af1ca30ba55b3eb4789976c
SmartDesign <mdsumonmiaup@gmail.com>
2020-10-29 11:26:59 +0600

    companycasual

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+
+
+});
+
+// collapsible 
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+
+// Code for browse by brand 
+var coll = document.getElementsByClassName("browse-brand");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("show-brand");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
